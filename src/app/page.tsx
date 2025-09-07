9f4c36f7f8d1e71c2f7432e6be918f8ace2b36d0
Wérik P Nascimento <116517426+WerikNascimento@users.noreply.github.com>
2025-09-06 22:43:43 -0300

    Atualizar o page.tsx
    sintaxe error ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/sections/hero";
 import { About } from "@/components/sections/about";
 import { Projects } from "@/components/sections/projects";
-import { Link } fron "next/link"
+import { Link } from "next/link"
 
 export default function Home() {
   return (
