ed2cda31a4f872f2db00b0d4e29f2a6902ed34c1
Wérik P Nascimento <116517426+WerikNascimento@users.noreply.github.com>
2025-09-06 22:40:27 -0300

    Atualizar o page.tsx
    Ajuste erro linkagem next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Hero } from "@/components/sections/hero";
 import { About } from "@/components/sections/about";
 import { Projects } from "@/components/sections/projects";
+import { Link } fron "next/link"
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <About />
-      <Projects />
+     <div>
+      <Link href="/hero">Hero</Link>
+     </div>
+     <div>
+      <Link href="/about">About</Link>
+     </div>
+     <div>
+      <Link href="/projects">projects</Link>
+     </div>
     </>
   );
 }
